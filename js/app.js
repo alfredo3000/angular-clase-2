24ac2e8fc875405b047aeb2cb71bd391b2eefff4
Estudiante <Estudiante>
2017-03-14 22:05:49 -0500

    formulario, constantes y servicios

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 var app = angular.module('miapp', ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider){
@@ -11,71 +10,149 @@ app.config(function($routeProvider, $locationProvider){
 			templateUrl: 'views/accounts.html',
 			controller: 'AccountsController'
 		})
-
 		.when('/add-account', {
 			templateUrl: 'views/add-account.html',
 			controller: 'AddAccountController'
-		})
-
+		})		
 		.otherwise('/');
 
 	$locationProvider.hashPrefix("");
 });
 
-app.service('AccountService', function(){
+app.constant('Config', {
+	'BASE': 'http://192.168.1.35:3039/', 
+	'ACCOUNTS': 'accounts/',
+});
+
+
+app.service('AccountService',function(Config, $q, $http){
 	var accounts = [
 		{ id:1, firstname: 'Edmundo', lastname: 'Acosta', mail: 'eacosta@atypax.net', accountNumber:'189-123123123-1', amount:'8000.99', currency:'USD$' },
 		{ id:2, firstname: 'Richard', lastname: 'Solis', mail: 'rsolis@atypax.net', accountNumber:'189-123123896-1', amount:'1.01', currency:'S/.' },
 		{ id:3, firstname: 'Esteban', lastname: 'Torres', mail: 'etorres@atypax.net', accountNumber:'189-123223123-1', amount:'999999.99', currency:'USD$' },
 		{ id:4, firstname: 'Erving', lastname: 'Córdova', mail: 'ecordova@atypax.net', accountNumber:'189-789987666-1', amount:'533.34', currency:'S/.' }
 	];
-  	
-  	function getAccounts(){
-  		return accounts;
-  	}
 
-  	return {
-  		listar: getAccounts
-  	}
-});     
+	function getAccounts(){ 
+		var deferred = $q.defer();
+		$http.get(Config.BASE + Config.ACCOUNTS)
+			.then(function(response){
+				deferred.resolve(response);
+			});
+		return deferred.promise;
+		
+	}
+	function saveAccount(params){
+		var deferred = $q.defer();
+		$http.post(Config.BASE + Config.ACCOUNTS, params)
+			.then(function(response){
+				deferred.resolve(response);
+			});
+		//var last =accounts[accounts.length-1].id;
+		//params.id = last+1;
+		//accounts.push(params);
+		return deferred.promise;
+	}
+
+	function deleteAccount(id){
+		/*
+		var index;
+		for(var i=0; i< accounts.length; i++){
+			if(accounts[i].id==id){
+				index =i;
+				break;
+			}
+
+		}
+		if(index){
+			accounts.splice(index, 1);
+		}
+		*/
+
+		var deferred = $q.defer();
+		$http.delete(Config.BASE + Config.ACCOUNTS + id)
+			.then(function(response){
+				deferred.resolve(response);
+			});
+
+		return deferred.promise;
+
+	}
+
+
+	return{
+		listar:getAccounts,
+		grabar:saveAccount,
+		eliminar: deleteAccount
+
+	}
+});
 
 app.controller('MainController', function($scope){
 	$scope.showMenu = false;
 	$scope.toggleMenu = function(){
 		$scope.showMenu = !$scope.showMenu;
-
 	}
-
 });
 
 app.controller('HomeController', function($scope){
 
 });
 
-app.controller('AccountsController', function($scope, AccountService){
-	$scope.lista = AccountService.listar(); 
+app.controller('AccountsController', function($scope,AccountService){
+	//$scope.lista =AccountService.listar();
+	function listar(){
+		AccountService.listar()
+			.then(function(response){
+				console.log(response);
+				$scope.lista = response.data.data;
+			});
+
+	}
+
+	$scope.onDelete = function(id){
+		AccountService.eliminar(id)
+			.then(function(response){
+				if(response.data.status==1){
+					listar();
+				}else{
+					alert('Ocurrió Error');
+				}
+			});
+
+		$scope.lista = AccountService.listar();
+
+	}
 	
+	listar();
+
 });
 
-app.controller('AddAccountController', function($scope){
-	$scope.cuenta = {
-		currency : '',
+app.controller('AddAccountController',function($scope,AccountService,$location){
+	$scope.cuenta ={
+		currency: '',
 	}
-
-	$scope.onSubmit = function(){
-
-		console.log('crear');
-
-	}
-
-	$scope.tipo_moneda=[  
-	{denominacion:'USD$', moneda:'Dólares Americanos'}, 
-	{denominacion:'S/.', moneda:'Nuevos Soles'}, 
-	
+	$scope.tipo_moneda=[
+	{ denominacion: 'USD$',moneda:'Dólares americanos'},
+	{ denominacion: 'S/.',moneda:'Nuevos soles'},	
 	];
- 
-});
+	$scope.onSubmit = function(){
+		console.log('crear');
+	
 
+		AccountService.grabar($scope.cuenta)
+			.then(function(response){
+				if(response.data.status ==1){
+					$location.path('/accounts');
+				}else{
+					alert('Ocurrio Error');
+				}
+			})
+		}
+
+		//$location.path('/accounts');
+	
+});
 
 app.directive('a', function(){
 	return {
@@ -90,34 +167,12 @@ app.directive('a', function(){
 	}
 });
 
-
-app.filter('reverse', function(){
+app.filter('reverse',function(){
 	return function(input){
-
-		var salida=''
-		for(var i=0; i<input.length; i++){
-
+		var	salida='';
+		for (var i = 0;i < input.length;i++) {
 			salida = input.charAt(i) + salida;
 		}
 		return salida;
 	}
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
